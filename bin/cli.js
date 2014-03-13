67742f13ae15b2901e6c199d49f62552d5a03529
root <jonathansanchez@me.com>
2014-03-13 11:22:04 +0100

    working on package manager

--- bin/cli.js
+++ bin/cli.js
@@ -17,6 +17,7 @@ program
 
 
 function run (host, tasks) {
+    // TODO exit with higther exit code
     var shell = new Shell();
     shell.connect(host).then(function () {
         if (!Array.isArray(tasks)) {
